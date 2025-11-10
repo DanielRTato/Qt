fd34e257aa9700664148b7fd8e2dc3f1843f5dce
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-11-10 14:41:28 +0100

    Roles de la tabla

--- Tabla/modeloTabla.py
+++ Tabla/modeloTabla.py
@@ -16,4 +16,9 @@ class ModeloTabla(QAbstractTableModel):
     def data(self, indice, rol):
         if rol == Qt.ItemDataRole.DisplayRole:
             return str(self.taboa[indice.row()][indice.column()])
+        if rol == Qt.ItemDataRole.BackgroundRole:
+            if self.taboa[indice.row()][2] == "Masculino":
+                return QtGui.QColor ("lightblue")
+            elif self.taboa[indice.row()][2] == "Feminino":
+                return QtGui.QColor ("lightpink")
 
